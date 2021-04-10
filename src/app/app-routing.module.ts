e341810cf3fa6f3c94356162145f6a192b2a0268
JyunCun <JyunCun@smart-ecare.com>
2021-04-10 15:59:45 +0800

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
@@ -8,7 +9,7 @@ import { Page2Component } from './page2/page2.component';
 const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
   { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
+  { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
   { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) },
